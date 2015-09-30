f919ce0bc8819155ffc8dd681abf6ba850312328
bubl-b <andr.bul@gmail.com>
2015-10-01 00:08:27 +0300

    rename files

--- azure-tests.py
+++ azure-tests.py
@@ -5,4 +5,12 @@ from azure.servicemanagement import *
 subscription_id = 'e225e7e0-8788-44dc-8586-481ee5cbad17'
 certificate_path = '../mycert.pem'
 
-#sms = ServiceManagementService(subscription_id, certificate_path)
+sms = ServiceManagementService(subscription_id, certificate_path)
+
+result = sms.list_locations()
+for location in result:
+    print(location.name)
+images = sms.list_vm_images()
+for image in images:
+    print image.label
+    #print image.__dict__
