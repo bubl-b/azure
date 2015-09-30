d5a79cd542f8042dc33f65217b3c51ba21cac0e4
bubl-b <andr.bul@gmail.com>
2015-09-30 23:13:49 +0300

    added azure.py

---
+++ azure.py
@@ -0,0 +1,8 @@
+#!/usr/bin/pyton
+from azure import *
+from azure.servicemanagement import *
+
+subscription_id = '<your_subscription_id>'
+certificate_path = '<path_to_.pem_certificate>'
+
+sms = ServiceManagementService(subscription_id, certificate_path)
